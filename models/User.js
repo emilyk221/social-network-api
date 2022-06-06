40a4116e7443516c86bfdc17f035bfa1bacdc036
emilyk <emily.g.kruse@gmail.com>
2022-06-06 11:30:25 -0400

    complete user, friend, thought routes

--- models/User.js
+++ models/User.js
@@ -27,7 +27,7 @@ const UserSchema = new Schema({
   friends: [
     {
       type: Schema.Types.ObjectId,
-      ref: 'Friend'
+      ref: 'User'
     }
   ]
 },
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -4,16 +4,47 @@ const { User, Thought } = require("../../models");
 // get all thoughts
 router.get('/', (req, res) => {
   Thought.find({})
-    .populate({
-      path: 'users',
-      select: '-__v'
-    })
     .select('-__v')
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbThoughtData => res.json(dbThoughtData))
     .catch(err => {
       console.log(err);
       res.status(400).json(err);
     });
+});
+
+router.get('/:id', ({ params }, res) => {
+  Thought.findOne({ _id: params.id })
+    .select('-__v')
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'No thought found with this id!'});
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.post('/', ({ body }, res) => {
+  Thought.create(body)
+    .then(({ _id }) => {
+      return User.findOneAndUpdate(
+        { _id: body.userId },
+        { $push: { thoughts: _id } },
+        { new: true }
+      );
+    })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id!' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
 });
 
 module.exports = router;
